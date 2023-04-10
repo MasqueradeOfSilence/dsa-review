6a5ebf1ed2cec9210244fbf1f1445316cd6d6a64
Alex <alexcn711@gmail.com>
2023-04-10 08:02:11 -0600

    adding queue test

---
+++ queue/src/QueueTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    Enqueue, dequeue, isFull, isEmpty, peek
+*/
+class QueueTest
+{
+    public void testEnqueueShouldAddANewElement()
+    {
+        
+    }
+}
