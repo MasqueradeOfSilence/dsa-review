35bdb1e2f52f73827dbd9ad2296d7428c9c751ae
Alex <alexcn711@gmail.com>
2023-08-04 10:38:51 -0600

    ref

--- priority_queue/src/com/evenstar/Main.java
+++ priority_queue/src/com/evenstar/Main.java
@@ -1,5 +1,9 @@
 package com.evenstar;
 
+/**
+ * Heapify reference: https://builtin.com/software-engineering-perspectives/heapify-heap-tree-cpp 
+ */
+
 public class Main
 {
     public static void main(String[] args)
