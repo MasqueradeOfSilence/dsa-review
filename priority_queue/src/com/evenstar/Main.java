25f742e2037a2db65cacfce4daa12ead495a6874
Alex <alexcn711@gmail.com>
2023-08-08 10:38:07 -0600

    creating pq class

--- priority_queue/src/com/evenstar/Main.java
+++ priority_queue/src/com/evenstar/Main.java
@@ -1,13 +1,16 @@
 package com.evenstar;
 
 /**
- * Heapify reference: https://builtin.com/software-engineering-perspectives/heapify-heap-tree-cpp 
+ * Heapify reference: https://builtin.com/software-engineering-perspectives/heapify-heap-tree-cpp
  */
 
 public class Main
 {
+    private static PriorityQueue priorityQueue;
+    
     public static void main(String[] args)
     {
         System.out.println("Hello priority queue!");
+        priorityQueue = new PriorityQueue();
     }
 }
