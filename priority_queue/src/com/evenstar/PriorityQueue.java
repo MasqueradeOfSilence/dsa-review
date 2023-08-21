4766a6e355474c692867c511dc2303f12efc9153
MasqueradeOfSilence <alexcn711@gmail.com>
2023-08-21 10:53:50 -0700

    pqueue progress

--- priority_queue/src/com/evenstar/PriorityQueue.java
+++ priority_queue/src/com/evenstar/PriorityQueue.java
@@ -2,6 +2,7 @@ package com.evenstar;
 
 // It doesn't like com.evenstar on IntelliJ Mac, so I unchecked "Wrong package statement"
 // https://stackoverflow.com/questions/26440623/package-name-does-not-correspond-to-the-file-path-intellij
+// Also note that this is not the same thing as Java's priority queue class
 public class PriorityQueue
 {
     // The max heap data member should not need to be exposed to other classes at any point.
