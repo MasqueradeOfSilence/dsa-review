8ab1bc1e22140cae94bc5b070c9fdc276d9dc8d4
Alex <alexcn711@gmail.com>
2023-09-12 09:13:30 -0600

    heapify cont

--- priority_queue/src/com/evenstar/MaxHeap.java
+++ priority_queue/src/com/evenstar/MaxHeap.java
@@ -6,6 +6,11 @@ public class MaxHeap
 {
     private ArrayList<Integer> nodes;
 
+    public MaxHeap(ArrayList<Integer> nodes)
+    {
+        this.nodes = nodes;
+    }
+
     public ArrayList<Integer> getNodes()
     {
         return nodes;
