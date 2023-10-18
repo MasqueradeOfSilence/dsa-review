c53729db9820ac2571b50bb639b336543b36e039
Alex <alexcn711@gmail.com>
2023-10-18 10:23:21 -0600

    pq

--- priority_queue/src/com/evenstar/MaxHeapTest.java
+++ priority_queue/src/com/evenstar/MaxHeapTest.java
@@ -1,3 +1,4 @@
+import com.evenstar.MaxHeap;
 import com.evenstar.PriorityQueue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,11 @@ class MaxHeapTest
     @Test
     public void testFindLargerChildShouldReturnTheLargerChild()
     {
-
+        MaxHeap maxHeap = new MaxHeap(new ArrayList<Integer>());
+        int child1 = 10;
+        int child2 = 8;
+        // should this be in a utils class instead?
+        int largerChild = maxHeap.findLargerChild(child1, child2);
+        assertEquals(largerChild, child1);
     }
 }
