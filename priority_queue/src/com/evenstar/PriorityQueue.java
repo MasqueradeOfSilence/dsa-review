bae23c9ba8932083d8486e96bd0b4d3972fd5438
Alex <alexcn711@gmail.com>
2023-11-17 18:35:21 -0700

    pq

--- priority_queue/src/com/evenstar/PriorityQueue.java
+++ priority_queue/src/com/evenstar/PriorityQueue.java
@@ -15,6 +15,7 @@ public class PriorityQueue
         maxHeap = new com.evenstar.MaxHeap(nodeValues);
     }
 
+    // TODO this is not needed. We need normal queue operations here.
     public void heapify()
     {
         maxHeap.heapify();
