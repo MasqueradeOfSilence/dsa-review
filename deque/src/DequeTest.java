4d8c6d4303683364e39936d3ced31dbe1d03d411
Alex <alexcn711@gmail.com>
2024-01-17 11:00:29 -0700

    insert rear stub

--- deque/src/DequeTest.java
+++ deque/src/DequeTest.java
@@ -27,4 +27,10 @@ class DequeTest
         assertEquals(expectedFourthElement, actualFourthElement);
         assertEquals(expectedLastElement, actualLastElement);
     }
+
+    @org.junit.jupiter.api.Test
+    void insertRearShouldPutIntegerAtRearOfDeque()
+    {
+        
+    }
 }
