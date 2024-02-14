1e1c7199cfb2c5bf17c1796cdda38c1311d6d79e
Alex <alexcn711@gmail.com>
2024-02-13 18:05:54 -0700

    unit test begin

---
+++ linked_list/src/LinkedListTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() 
+    {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void traverse()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertFront()
+    {
+    }
+}
