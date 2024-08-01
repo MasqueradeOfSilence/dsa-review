08e06e10076a0a96fc671d7d5485abd1e8171066
Alex <alexcn711@gmail.com>
2024-08-01 09:42:12 -0600

    Circular queue progress

--- circular_queue/src/CircularQueue.java
+++ circular_queue/src/CircularQueue.java
@@ -1,4 +1,11 @@
+/**
+ * My implementation of a circular queue
+ *
+ * Do not use lists
+ */
+
 public class CircularQueue
 {
-
+    private int currentFrontPosition = -1;
+    private int currentRearPosition = -1;
 }
