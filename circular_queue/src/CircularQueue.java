23732ff2cc12e8beaab6231219f7375e91c8004c
Alex <alexcn711@gmail.com>
2024-08-14 08:45:33 -0600

    progress on circ queue

--- circular_queue/src/CircularQueue.java
+++ circular_queue/src/CircularQueue.java
@@ -8,4 +8,6 @@ public class CircularQueue
 {
     private int currentFrontPosition = -1;
     private int currentRearPosition = -1;
+    private int QUEUE_SIZE = 10;
+    private int items[] = new int[QUEUE_SIZE];
 }
