13877ec0b32f19580bf2cdb46484a8aa5aed257b
MasqueradeOfSilence <alexcn711@gmail.com>
2024-12-09 10:34:23 -0700

    DLL node

--- double_linked_list/src/DoubleLinkedList.java
+++ double_linked_list/src/DoubleLinkedList.java
@@ -5,5 +5,18 @@
  */
 public class DoubleLinkedList
 {
+    Node head;
+    static class Node
+    {
+        int value;
+        Node next;
+        Node previous;
 
+        Node(int value)
+        {
+            this.value = value;
+            next = null;
+            previous = null;
+        }
+    }
 }
