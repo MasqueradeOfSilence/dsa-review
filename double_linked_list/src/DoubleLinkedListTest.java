b57cae0406ab84092605862e16488baa83253cad
MasqueradeOfSilence <alexcn711@gmail.com>
2024-12-12 09:02:59 -0700

    wip dll

--- double_linked_list/src/DoubleLinkedListTest.java
+++ double_linked_list/src/DoubleLinkedListTest.java
@@ -5,6 +5,11 @@ class DoubleLinkedListTest
     @org.junit.jupiter.api.Test
     void insertFront()
     {
-
+        DoubleLinkedList dll = new DoubleLinkedList();
+        int data1 = 6;
+        int data2 = 1924;
+        dll.insertFront(data1);
+        dll.insertFront(data2);
+        
     }
 }
