85bf4be710e2fb58eb53ba932dd991b556bdb1b7
MasqueradeOfSilence <alexcn711@gmail.com>
2024-12-17 08:48:18 -0700

    insertAfter dll

--- double_linked_list/src/DoubleLinkedListTest.java
+++ double_linked_list/src/DoubleLinkedListTest.java
@@ -17,6 +17,20 @@ class DoubleLinkedListTest
     @org.junit.jupiter.api.Test
     void insertAfter()
     {
-
+        DoubleLinkedList dll = new DoubleLinkedList();
+        int data1 = 6;
+        int data2 = 1924;
+        dll.insertFront(data1);
+        dll.insertFront(data2);
+        int data3 = 666;
+        dll.insertAfter(dll.head, data3);
+        // 1924 -> 666 -> 6
+        int data4 = 10;
+        dll.insertAfter(dll.head.next.next, data4);
+        // 1924 -> 666 -> 6 -> 10
+        assertEquals(dll.head.value, data2);
+        assertEquals(dll.head.next.value, data3);
+        assertEquals(dll.head.next.next.value, data1);
+        assertEquals(dll.head.next.next.next.value, data4);
     }
 }
