93a4a981e2b3e885d6a6c50d7cb56b8b6ef1451e
MasqueradeOfSilence <alexcn711@gmail.com>
2024-12-22 10:55:37 -0800

    wip dll

--- double_linked_list/src/DoubleLinkedList.java
+++ double_linked_list/src/DoubleLinkedList.java
@@ -48,6 +48,8 @@ public class DoubleLinkedList
     public void insertEnd(int data)
     {
         Node newNode = new Node(data);
+        Node temp = head;
+        newNode.next = null;
         
     }
 
