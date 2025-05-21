05e2b915f1d459167e7c68f748f55374256c2467
MasqueradeOfSilence <alexcn711@gmail.com>
2025-05-21 08:45:22 -0600

    adding link for bucket hash

--- hash_table/src/HashTable.java
+++ hash_table/src/HashTable.java
@@ -1,6 +1,7 @@
 /**
  * Hash Table
  * https://www.programiz.com/dsa/hash-table
+ * Bucket Hashing: https://www.geeksforgeeks.org/implementing-our-own-hash-table-with-separate-chaining-in-java/
  *
  * A hash table exists to store elements. It stores them as key-value pairs for faster data retrieval.
  *
